c7dda15a4b799c0167f6a940c4d016d202abb3f8
Sousa <alisson.raniel.sousa@accenture.com>
2016-06-20 21:12:08 -0300

    commit do projeto de android

--- SiNewsApplication/app/src/main/java/com/pdp/si/facisa/cesed/sinewsapplication/MainActivitySiNews.java
+++ SiNewsApplication/app/src/main/java/com/pdp/si/facisa/cesed/sinewsapplication/MainActivitySiNews.java
@@ -1,5 +1,6 @@
 package com.pdp.si.facisa.cesed.sinewsapplication;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivitySiNews extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private WebView myWeb;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,7 @@ public class MainActivitySiNews extends AppCompatActivity
         setContentView(R.layout.activity_main_si_news);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +56,75 @@ public class MainActivitySiNews extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        myWeb = (WebView) findViewById(R.id.webview);
+        myWeb.getSettings().setLoadsImagesAutomatically(true);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.getSettings().setUseWideViewPort(true);
+        myWeb.getSettings().setLoadWithOverviewMode(true);
+        myWeb.getSettings().setSupportZoom(true);
+        myWeb.getSettings().setBuiltInZoomControls(true);
+        myWeb.getSettings().setDisplayZoomControls(false);
+        myWeb.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        myWeb.setWebViewClient(new MyBrowser());
+        String path = Uri.parse("file:///assets/index.html").toString();
+        myWeb.loadUrl(path);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MainActivitySiNews Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.pdp.si.facisa.cesed.sinewsapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MainActivitySiNews Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.pdp.si.facisa.cesed.sinewsapplication/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @JavascriptInterface
+    public void showToast() {
+
     }
 
     @Override
